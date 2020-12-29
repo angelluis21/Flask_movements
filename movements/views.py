d5927dbbf1fcba2c16db0cf13a7c879953ebe476
Angelluis21 <anyel_luis21@hotmail.com>
2020-12-29 19:22:11 +0100

    Ciclo de llamada a crea alta

--- movements/views.py
+++ movements/views.py
@@ -1,8 +1,21 @@
 from movements import app
 from flask import render_template 
-
-import random
+import csv
 
 @app.route('/')
-def listaMovimientos():  #También puede llamarse --> def index():
-    return render_template("movementsList.html")
+def listaIngresos():      #También puede llamarse --> def index():
+    fIngresos = open("movements/data/basededatos.csv", "r")
+    csvReader = csv.reader(fIngresos, delimiter=',', quotechar='"')
+    ingresos = list(csvReader)
+
+    sumador = 0
+    for ingreso in ingresos:
+        sumador += float(ingreso[2])
+
+    print(ingresos) #Ponemos este 'print' para ver en la consola. No hay que ponerlo para el código.
+
+    return render_template("movementsList.html", datos=ingresos, total=sumador)
+
+@app.route('/creaalta')
+def nuevoIngreso():
+    return 'Ya el miercoles si eso te enseño el formulario'
